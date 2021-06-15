f2efda4aa9fb36be20fb5db81e62b02f9e8f21b5
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-15 02:44:14 -0400

    setting up app return

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,25 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Search from "./pages/Search";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import List from "./components/List"
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    <div class = "App">
+      <Header />
+        <Search />
+        <List>
+
+        </List>
+
+    </div>
+    
   );
 }
 
